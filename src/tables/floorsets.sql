69327b5de6ec80f9b08ae2381796ee84a177bfe9
andyfult <fulton_family@yahoo.com>
2025-01-29 22:53:42 -0500

    Changed "Layouts" to "Floorsets"

--- src/tables/floorsets.sql
+++ src/tables/floorsets.sql
@@ -3,13 +3,13 @@ Filename: layouts.sql
 Part of Project: PLOT/PLOT-DB/src/tables
 
 File Purpose:
-This file contains the commands to create the Layouts table for the database.
+This file contains the commands to create Floorsets table for the database.
 
 Written by: Andrew Fulton
 */
 
---Create Layouts Table
-CREATE TABLE Layouts (
+--Create Floorsets Table
+CREATE TABLE Floorsets (
     TUID INT PRIMARY KEY,
     NAME VARCHAR(100) NOT NULL,
     STORE_TUID INT NOT NULL,
@@ -17,6 +17,6 @@ CREATE TABLE Layouts (
     CREATED_BY INT NOT NULL,
     DATE_MODIFIED DATETIME NOT NULL,
     MODIFIED_BY INT NOT NULL,
-    FOREIGN KEY (STORE_TUID) REFERENCES Stores(TUID)
+    FOREIGN KEY (STORE_TUID) REFERENCES Store(TUID)
 );
 GO
