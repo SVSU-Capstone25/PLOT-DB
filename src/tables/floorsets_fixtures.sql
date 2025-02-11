fea35e012d9a9db6f833113e9d3554864826cb27
Andrew Miller <chumley207@gmail.com>
2025-02-11 02:28:50 -0500

    Added Allocated_LF, Category, and Note columns to floorsets_fixtures table.

--- src/tables/floorsets_fixtures.sql
+++ src/tables/floorsets_fixtures.sql
@@ -8,6 +8,7 @@ table for the database.
 
 Association table for the floorsets and fixture tables.
 Provides position of a given fixture on a given floorset.
+Provides allocated linear feet, and category of merchandise.
 
 Written by: Andrew Miller
 */
@@ -18,6 +19,16 @@ CREATE TABLE Floorsets_Fixtures (
 	FIXTURE_TUID INT,							/* TUID of Fixture */
 	X_POS DECIMAL(9,6) NOT NULL,				/* X-Coordinate */
 	Y_POS DECIMAL(9,6) NOT NULL,				/* Y-Coordinate */
+
+	/* linear feet allocated to fixture */
+	ALLOCATED_LF DECIMAL(10,2),
+
+	/* Category of merchandise on fixture */					
+	CATEGORY VARCHAR(100),
+
+	/* Optional note, defaults as empty string */						
+	NOTE VARCHAR(1000) NOT NULL DEFAULT '',
+	
 	PRIMARY KEY(FLOORSET_TUID, FIXTURE_TUID),
 	FOREIGN KEY(FLOORSET_TUID) REFERENCES Floorsets(TUID),
 	FOREIGN KEY(FIXTURE_TUID) REFERENCES Fixtures(TUID)
