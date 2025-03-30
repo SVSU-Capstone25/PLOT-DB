3b0646537fe50cdacb296fd984c2a7a245dc8b0d
Andrew Miller <chumley207@gmail.com>
2025-03-29 23:52:59 -0400

    Bug Fix. Using "Instead Of Delete" statements.

--- src/triggers/trg_DeleteForeignKeysForFixtureOnDelete.sql
+++ src/triggers/trg_DeleteForeignKeysForFixtureOnDelete.sql
@@ -5,18 +5,23 @@ Part of Project: PLOT/PLOT-DB/src/triggers
 File Purpose:
 When a fixture is deleted from fixtures table,
 delete the fixture from the floorsets_fixtures table
+Note: Uses "Instead of Delete" and does the delete at the end.
 
 Written by: Andrew Miller
 */
 
 CREATE TRIGGER trg_DeleteForeignKeysForFixtureOnDelete
 ON dbo.FIXTURES
-AFTER DELETE
+INSTEAD of DELETE
 AS
 BEGIN
 	/* Delete fixture records in the floorsets-fixtures
 	association table when its TUID matches that deleted in floorsets */
     DELETE FROM FLOORSETS_FIXTURES
     WHERE FIXTURE_TUID IN (SELECT TUID FROM deleted);
+
+    -- Delete from fixtures table
+	DELETE FROM FIXTURES
+	WHERE TUID IN (SELECT TUID FROM deleted);
 END;
 GO
