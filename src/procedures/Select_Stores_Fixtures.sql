4699c34882d5e33b885e1d87c30e44bef44ec451
Zach Ventimiglia <zjventim@svsu.edu>
2025-04-04 17:29:53 -0400

    Committing changes from Azure DB

---
+++ src/procedures/Select_Stores_Fixtures.sql
@@ -0,0 +1,34 @@
+/****** Object:  StoredProcedure [dbo].[Select_Stores_Fixtures]    Script Date: 4/4/2025 5:27:37 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Zach Ventimiglia      
+-- Create Date: 4/1/2025
+-- Description: 
+-- This select grabs all the fixture models for a store.
+-- =============================================
+
+CREATE   PROCEDURE [dbo].[Select_Stores_Fixtures]
+(
+	@StoreID INT = NULL
+)
+AS
+BEGIN
+	IF @StoreID IS NOT NULL
+	BEGIN
+		SELECT *
+		FROM Fixtures
+		WHERE STORE_TUID = @StoreID
+	END
+	ELSE
+	BEGIN
+		SELECT 'Error 500' AS Response
+	END
+END
+GO
+
+
