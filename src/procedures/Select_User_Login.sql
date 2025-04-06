31baf5ea05168f6f312c9e49328a07113ba392ae
Zach Ventimiglia <zjventim@svsu.edu>
2025-04-06 15:27:30 -0400

    Fixed if an email doesnt exist, it will throw an error 404 not found

--- src/procedures/Select_User_Login.sql
+++ src/procedures/Select_User_Login.sql
@@ -14,26 +14,38 @@ GO
 
 CREATE OR ALTER PROCEDURE [dbo].[Select_User_Login]
 (
-	@Email VARCHAR(320) = NULL
+	@EMAIL VARCHAR(320) = NULL
 )
 AS
 BEGIN
-	IF @Email IS NOT NULL
+	IF @EMAIL IS NOT NULL
 	BEGIN
-		SELECT 
-			TUID,
-			FIRST_NAME,
-			LAST_NAME,
-			EMAIL,
-			PASSWORD,
-			(
-				SELECT NAME 
-				FROM Roles 
-				WHERE TUID = ROLE_TUID
-			) AS 'ROLE',
-			ACTIVE
-		FROM Users
-		WHERE EMAIL = @EMAIL
+		-- Checking if the email exists
+		IF EXISTS (
+        SELECT 1 
+            FROM Users
+            WHERE EMAIL = @EMAIL
+        ) 
+		BEGIN
+			SELECT 
+				TUID,
+				FIRST_NAME,
+				LAST_NAME,
+				EMAIL,
+				PASSWORD,
+				(
+					SELECT NAME 
+					FROM Roles 
+					WHERE TUID = ROLE_TUID
+				) AS 'ROLE',
+				ACTIVE
+			FROM Users
+			WHERE EMAIL = @EMAIL
+		END
+		ELSE
+		BEGIN
+			SELECT 'Error 404: Email not found' AS Response
+		END
 	END
 	ELSE
 	BEGIN
