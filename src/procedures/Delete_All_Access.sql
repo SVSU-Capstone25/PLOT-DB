7639052f9a2ec485742a9878e1c4ed67ce6b8723
Zach Ventimiglia <zjventim@svsu.edu>
2025-04-07 16:57:56 -0400

    updated insert users SP and added delete user stores access SP

---
+++ src/procedures/Delete_All_Access.sql
@@ -0,0 +1,30 @@
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Zach Ventimiglia      
+-- Create Date: 2/13/2025
+-- Description: 
+-- This procedure deletes all of the users stores access'
+-- =============================================
+CREATE PROCEDURE [dbo].[Delete_All_Access]
+(
+	@USER_TUID INT = NULL
+)
+AS 
+BEGIN
+	BEGIN TRY
+		-- Delete store from access table for that user
+		DELETE FROM Access
+		WHERE USER_TUID = @USER_TUID
+			
+		SELECT 'OK 200' As Response;
+	END TRY
+	BEGIN CATCH
+		-- Handle errors
+		SELECT ERROR_MESSAGE() As Response;
+	END CATCH
+END
