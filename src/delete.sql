55c93b7210292e9c164e9833ca3347cabdac0f42
Clayton Cook <claytonleonardcook+github@gmail.com>
2025-04-15 01:26:48 -0400

    reorder delete sql based on dependency

--- src/delete.sql
+++ src/delete.sql
@@ -1,22 +1,32 @@
 IF OBJECT_ID('Sales_Allocation', 'U') IS NOT NULL DROP TABLE [dbo].[Sales_Allocation];
 GO
+
+IF OBJECT_ID('Employee_Area', 'U') IS NOT NULL DROP TABLE [dbo].[Employee_Area];
+GO
+
 IF OBJECT_ID('Sales', 'U') IS NOT NULL DROP TABLE [dbo].[Sales];
 GO
+
 IF OBJECT_ID('Floorsets_Fixtures', 'U') IS NOT NULL DROP TABLE [dbo].[Floorsets_Fixtures];
 GO
+
 IF OBJECT_ID('Floorsets', 'U') IS NOT NULL DROP TABLE [dbo].[Floorsets];
 GO
+
 IF OBJECT_ID('Fixtures', 'U') IS NOT NULL DROP TABLE [dbo].[Fixtures];
 GO
+
 IF OBJECT_ID('Access', 'U') IS NOT NULL DROP TABLE [dbo].[Access];
 GO
+
 IF OBJECT_ID('Users', 'U') IS NOT NULL DROP TABLE [dbo].[Users];
 GO
+
 IF OBJECT_ID('Stores', 'U') IS NOT NULL DROP TABLE [dbo].[Stores];
 GO
+
 IF OBJECT_ID('Roles', 'U') IS NOT NULL DROP TABLE [dbo].[Roles];
 GO
+
 IF OBJECT_ID('Supercategories', 'U') IS NOT NULL DROP TABLE [dbo].[Supercategories];
-GO
-IF OBJECT_ID('Employee_Area', 'U') IS NOT NULL DROP TABLE [dbo].[Employee_Area];
 GO
